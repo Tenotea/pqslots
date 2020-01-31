c93951aac972c995cc8433b573e46453fa16c34d
Tenotea <drtenotea@gmail.com>
2020-01-31 15:08:03 +0100

    nav pane included for android

--- home/home.js
+++ home/home.js
@@ -1,6 +1,3 @@
-//width="631.765" height="184.603"
-
-
 const body = document.querySelector('body');
 const loadingPage = document.querySelector('.first-loading-page');
 const siteIntro = document.querySelector('.intro-page');
@@ -9,34 +6,83 @@ const logoEffect = document.querySelector('#logo path');
 const enterText = document.querySelector('.t1');
 const enterText2 = document.querySelector('.t2');
 const finishLine = document.querySelector('.finish-line'); 
-const menuIcon = document.querySelector('#menu-icon');
-const desktopNav = document.querySelector('.desktop-nav');
-const navOptions = document.querySelector('.nav-options');
-const mobileNavigation = document.querySelector('.navigation');
-
-let logoWidth = () => {
-    let screenSize = (window.innerWidth * 60)/100;
-    Math.ceil(screenSize);
-    logo.style.width = screenSize;
-
-    if(screenSize++ || screenSize--){
-        logo.style.width = screenSize;
-    }    
-}
-
-logoWidth();
 
 
+//........THE PAGE LOADS HERE.........//
 let loadingEffect = () => {
     loadingPage.style.transform = "translateY(-100vh)";
     logoEffect.style.cssText = "animation: naming 5s ease-in-out 0s 1 alternate forwards;"
      enterText.style.cssText = "animation: text-enter01 1s 1 ease-in-out 3.6s;";
      enterText2.style.cssText= "animation: text-enter02 1s 1 ease-in-out 4.4s;";
-     console.log(window.scrollY);    
-} 
+    } 
 //setTimeout(loadingEffect, 5000);
 window.addEventListener('load', loadingEffect);
 
+
+
+
+
+
+//............SIDE_NAVIGATION_GOES_HERE..............//
+const navSlide = () => {
+    const menuIcon = document.querySelector('.burger');
+
+    menuIcon.addEventListener('click' , () =>{
+        const menuList = document.querySelector('.nav-links');
+        menuList.classList.toggle('nav-links-active');
+
+        const menuLinks = document.querySelectorAll('.nav-links li');
+        for(i = 0; i < menuLinks.length; i++){
+            if(menuLinks[i].style.animation){
+                menuLinks[i].style.animation = "";
+            }  else {
+                menuLinks[i].style.animation = `linksUp .3s 1 ease-in-out ${(i/5)}s forwards`;
+            }
+            
+        }
+
+});
+ 
+    }
+
+navSlide();
+
+//..............RESPONSIVE_HEADER_TO_USER_SCROLL.............//
+const header = document.querySelector('.navigation');
+let lastScrollPosition =  window.scrollY;
+
+window.addEventListener('scroll' , checkScrollDirection);
+function checkScrollDirection(){
+    let newScrollPosition = window.scrollY;
+    
+    if(newScrollPosition > lastScrollPosition){
+        console.log("going up");
+        header.style.cssText = "transform: translateY(-10vh); transition: transform .4s ease-in-out;"
+    } else if(newScrollPosition == lastScrollPosition){
+        return;  
+    }
+     else{
+        console.log("comming down");   
+        header.style.cssText = "transform: translateY(0vh); transition: transform .4s ease-in-out;"
+    }
+    lastScrollPosition = newScrollPosition;  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//.........THE LANDING PAGE APPEARS...........//
 enterText.addEventListener("webkitAnimationEnd", () => enterText.className += " add");
 enterText2.addEventListener("webkitAnimationEnd", () => {
     enterText2.className += " add";
@@ -45,38 +91,21 @@ enterText2.addEventListener("webkitAnimationEnd", () => {
 
 finishLine.addEventListener("webkitAnimationEnd" , () => {
     finishLine.style.cssText = "transform: translate(-50%, -50%) scale(1);";
-   desktopNav.style.cssText = "transform: translateY(-100vh); transition: transform .4s ease-in-out;"
-    // menuIcon.style.cssText = "z-index: 1;"
 })
 
 
+///...........THE SVG PRECAUTION.........//
+let logoWidth = () => {
+    let screenSize = (window.innerWidth * 60)/100;
+    Math.ceil(screenSize);
+    logo.style.width = screenSize;
+    if(screenSize++ || screenSize--){
+        logo.style.width = screenSize;
+    }    
+}
+logoWidth();
 
 
 
-//..............RESPONSIVE_HEADER_TO_USER_SCROLL.............//
-// let lastScrollPosition =  window.scrollY;
 
-// window.addEventListener('scroll' , checkScrollDirection);
-// function checkScrollDirection(){
-//     let newScrollPosition = window.scrollY;
-    
-//     if(newScrollPosition > lastScrollPosition){
-//         console.log("going up");
-//         desktopNav.style.cssText = "transform: translateY(-120vh); transition: transform .4s ease-in-out;"
-//     } else if(newScrollPosition == lastScrollPosition){
-//         return;  
-//     }
-//      else{
-//         console.log("comming down");   
-//         desktopNav.style.cssText = "transform: translateY(-100vh); transition: transform .4s ease-in-out;"
-//     }
-//     lastScrollPosition = newScrollPosition;  
-// }
 
-//..........ANDROID_NAV_SLIDE_IN..........//
-
-const androidMenu = document.querySelector('.android-menu');
-
-androidMenu.addEventListener('click', () => {
-    navOptions.style.cssText = "transform: translate(50vw, 47vh); transition: .6s transform ease-in-out;";
-})
